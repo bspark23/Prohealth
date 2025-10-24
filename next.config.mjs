814e007c6ba20a8dae01b5f629ce4f68b9d84d2b
Blessing <adaoma2826@gmail.com>
2025-10-24 05:20:14 -0300

    Remove PWA config to fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,3 @@
-import withPWAInit from '@ducanh2912/next-pwa';
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -21,4 +12,4 @@ const nextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
